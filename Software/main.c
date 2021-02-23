50980b2c1521f602d450a2359d4bbd813c820826
Steven Yik <stevenyik156184@gmail.com>
2021-02-23 01:10:17 -0500

    Working Display Driver Images are displayed from HEX contained in ImageData.c

--- Software/main.c
+++ Software/main.c
@@ -6,26 +6,34 @@
 #include "hardware/clocks.h"
 #include "hardware/rosc.h"
 #include "hardware/structs/scb.h"
+#include "EPD_Test.h"
 
 const uint LED_PIN = 25;
-const uint BUTTON_PIN = 10;
+// const uint BUTTON_PIN = 10;
+
+
 
 int main() {
   
   bi_decl(bi_program_description("Test Software for E-paper display"));
-  bi_decl(bi_1pin_with_name(LED_PIN, "On-board LED"));
 
   stdio_init_all();
-  printf("Starting Pi Pico!\n");
-  sleep_ms(5000);
 
   gpio_init(LED_PIN);
   gpio_set_dir(LED_PIN, GPIO_OUT);
 
-  sleep_run_from_xosc();
-  
-  sleep_goto_dormant_until_edge_high(10);
+  gpio_put(LED_PIN, 1);
+  printf("LED ON\n");
+  sleep_ms(5000);
+  gpio_put(LED_PIN, 0);
+  printf("LED OFF\n");
+  sleep_ms(5000);
 
+
+  EPD_7in5_V2_test();
+
+
+  
   while (1) {
     
     gpio_put(LED_PIN, 0);
